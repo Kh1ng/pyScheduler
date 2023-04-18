fa0e8be6a623dfb8ffe1a7449458c1200f398f18
kh1ng <khing@pm.me>
2023-04-18 09:01:38 -0500

    full project

---
+++ main.py
@@ -0,0 +1,12 @@
+# This is a sample Python script.
+from nerd import Nerd
+from schedule import Schedule
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+path = "."
+schedule = Schedule("April", path)
+nerd = Nerd("Eric")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
