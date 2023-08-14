adcfc0670e2679bc819c7f74d80c8af0cea9037e
Colton <khing@pm.me>
2023-08-14 12:19:01 -0500

    no more oop

--- nerd.py
+++
@@ -1,9 +0,0 @@
-class Nerd:
-    def __init__(self, name):
-        self.name = name
-        self.expected_shifts = 15 #totalShifts / numDoctors  = expected_per_ad_person
-        self.needOff = [] # import from excel sheet
-        self.isPa;
-
-    def __repr__(self):
-        return self.name
