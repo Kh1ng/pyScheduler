1c95d85beca4a00314abacef0c703f2cc59d055f
kh1ng <khing@pm.me>
2023-08-14 17:10:40 -0500

    writing day name, number and names

--- main.py
+++ main.py
@@ -6,24 +6,22 @@ wb = Workbook()
 # Select the worksheet
 worksheet = wb.active
 
+# Write the days to rows 1 (name) & 2 (number)
+# Get the days in the month and their abbreviated names
+days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+num_days = calendar.monthrange(2023, 4)
+
+# Write the days and their abbreviated names to rows 1 and 2, write month to A1
+for i in range(0, num_days[1]):
+    worksheet.cell(row=1, column=i + 2,
+                   value=(days[i % 7]))
+    worksheet.cell(row=2, column=i + 2, value=i + 1)
+
 nerds = ['Eric', 'Alice', 'Bob', 'Charlie', 'David', 'Joe']
 
 # Write the names to column A
 for i, name in enumerate(nerds, start=3):
     worksheet.cell(row=i, column=1, value=name)
-
-# Write the days to rows 1 (name) & 2 (number)
-# Get the days in the month and their abbreviated names
-days = calendar.month_abbr[1:]
-num_days = calendar.monthrange(2023, 4)
-days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-
-# Write the days and their abbreviated names to rows 1 and 2, write month to A1
-for i, num_days in enumerate(calendar.monthrange(2023, 4)):
-    worksheet.cell(row=1, column=i,
-                   value=(calendar.day_name[datetime.strptime(('2023-04-' + str((i - 1) % 7)), '%Y-%m-%d').date().weekday()])[
-                         :2])
-    worksheet.cell(row=2, column=i, value=i - 1)
 
 # Save the file
 wb.save("sample.xlsx")
