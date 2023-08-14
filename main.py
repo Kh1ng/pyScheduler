07b5b566a59c44334118502857076e1a73292a60
Colton <khing@pm.me>
2023-08-14 16:45:35 -0500

    testing

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-# This is a sample Python script.
-from nerd import Nerd
-from schedule import Schedule
+import calendar
+from datetime import datetime
+from openpyxl import Workbook
+wb = Workbook()
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Select the worksheet
+worksheet = wb.active
 
-path = "./"
-schedule = Schedule(4, path)
-nerd = Nerd("Eric")
+nerds = ['Eric', 'Alice', 'Bob', 'Charlie', 'David', 'Joe']
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Write the names to column A
+for i, name in enumerate(nerds, start=3):
+    worksheet.cell(row=i, column=1, value=name)
+
+# Write the days to rows 1 (name) & 2 (number)
+# Get the days in the month and their abbreviated names
+days = calendar.month_abbr[1:]
+num_days = calendar.monthrange(2023, 4)
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+# Write the days and their abbreviated names to rows 1 and 2, write month to A1
+for i, num_days in enumerate(calendar.monthrange(2023, 4)):
+    worksheet.cell(row=1, column=i,
+                   value=(calendar.day_name[datetime.strptime(('2023-04-' + str((i - 1) % 7)), '%Y-%m-%d').date().weekday()])[
+                         :2])
+    worksheet.cell(row=2, column=i, value=i - 1)
+
+# Save the file
+wb.save("sample.xlsx")
